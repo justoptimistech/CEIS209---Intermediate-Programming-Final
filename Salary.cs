c0a918d1f6000ce1564090c3d85d5c20a4948e2e
Sovanarry Top <155851957+justoptimistech@users.noreply.github.com>
2024-04-16 21:29:50 -0400

    Create Salary.cs

---
+++ Salary.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Top_CourseProject_part2
+{
+    internal class Salary : Employee
+    {
+        // attributes
+        private double annualSalary;
+        
+        // constructors
+        public Salary() : base() 
+        {
+            annualSalary = 0.0;            
+        }
+        public Salary(string firstName, string lastName,
+            string ssn, DateTime hireDate,
+            Benefits benefits, double annualSalary) : base(firstName, lastName, ssn, hireDate, benefits)
+        {
+            this.annualSalary = annualSalary;
+        }
+
+        // behaviors
+        public override double CalculatePay()
+        {
+            return annualSalary / 26.0;
+        }
+
+        public override string ToString() 
+        {
+            return base.ToString() + ", annualSalary=" + annualSalary.ToString("C2");
+        }
+
+        // properties
+        public double AnnualSalary 
+        {
+            get { return annualSalary; }
+            set { annualSalary = value; }
+        }
+    }
+}
