8a76ca37185d8351e45960ef82b0e4ad5b8a97da
Sovanarry Top <155851957+justoptimistech@users.noreply.github.com>
2024-04-21 14:11:45 -0400

    Update Salary.cs

--- Salary.cs
+++ Salary.cs
@@ -1,44 +1,85 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Top_CourseProject_part2
 {
-    public class Salary : Employee
+    public partial class InputForm : Form
     {
-        // attributes
-        private double annualSalary;
-
-        // constructors
-        public Salary() : base()
+        public InputForm()
         {
-            annualSalary = 0.0;
-        }
-        public Salary(string firstName, string lastName,
-            string ssn, DateTime hireDate,
-            Benefits benefits, double annualSalary) : base(firstName, lastName, ssn, hireDate, benefits)
-        {
-            this.annualSalary = annualSalary;
+            InitializeComponent();
         }
 
-        // behaviors
-        public override double CalculatePay()
+        private void SubmitButton_Click(object sender, EventArgs e)
         {
-            return annualSalary / 26.0;
+            this.DialogResult = DialogResult.OK;
+            this.Hide();
         }
 
-        public override string ToString()
+        private void ExitButton_Click(object sender, EventArgs e)
         {
-            return base.ToString() + ", annualSalary=" + annualSalary.ToString("C2");
+            this.DialogResult= DialogResult.Cancel;
+            this.Hide();
         }
 
-        // properties
-        public double AnnualSalary
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            get { return annualSalary; }
-            set { annualSalary = value; }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InputForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HourlyRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if(HourlyRadioButton.Checked)
+            {
+                // hide the salary items
+                SalaryLabel.Visible = false;
+                SalaryTextBox.Visible = false;
+
+                // show the hourly items
+                HourlyRateLabel.Visible = true;
+                HourlyRateTextBox.Visible = true;
+                HoursWorkedLabel.Visible = true;
+                HoursWorkedTextBox.Visible = true;
+
+            }
+        }
+
+        private void SalaryRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (SalaryRadioButton.Checked)
+            {
+                // show the salary items
+                SalaryLabel.Visible = true;
+                SalaryTextBox.Visible = true;
+
+                // hide the hourly items
+                HourlyRateLabel.Visible = false;
+                HourlyRateTextBox.Visible = false;
+                HoursWorkedLabel.Visible = false;
+                HoursWorkedTextBox.Visible = false;
+            }
         }
     }
 }
